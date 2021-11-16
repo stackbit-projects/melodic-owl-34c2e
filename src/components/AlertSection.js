22d1c220258d1f8c51e85ca65b3eac9faa31c4d1
Stackbit Code Editor <projects@stackbit.com>
2021-11-16 06:20:52 +0000

    AlertSection.js: changed

--- src/components/AlertSection.js
+++ src/components/AlertSection.js
@@ -6,3 +6,12 @@ const AlertSection = ({ body }) => {
 
 export default AlertSection
 
+const AlertSection = ({ annotationPrefix, body }) => {
+  return (
+    <div data-sb-field-path={annotationPrefix}>
+      <p data-sb-field-path=".body">{body}</p>
+    </div>
+  )
+}
+
+export default AlertSection
